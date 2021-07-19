b424f495db89a90bc1ec11412459554b73ce0e2f
ilminmoon <ian.ilminmoon@gmail.com>
2021-07-19 09:53:49 +0900

    vue update

---
+++ vue/src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/Signup.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+  },
+  {
+    path: "/secret",
+    name: "Secret",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/Secret.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
---
+++ vue/src/views/Secret.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="secret">
+    HELLO! I'm SECRET!
+    <!-- <div v-model="ss">{{this.ss}}</div> -->
+    {{ss}}
+    <input v-model="ss">
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  // new Vue({})
+  name: "Secret",
+  data() {
+    return {
+      ss: [],
+    }
+  },
+  mounted() {
+    console.log(localStorage.getItem('auth-token'));
+    axios
+        .post(
+          "http://119.56.162.178:8088/project1/api/jwt/secret", {},
+          {
+            headers: {"auth-token" : localStorage.getItem('auth-token')}
+          },
+          //http://119.56.162.178:8089/project1/swagger-ui/index.html
+        )
+        .then((response) => {
+          this.ss = response.data;
+        });
+  },
+};
+</script>
+
+<style></style>
