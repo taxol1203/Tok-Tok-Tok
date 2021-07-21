f9af04554ae7c6c76deeccd438d503df66ab9ccd
김소빈 <ksbjjang333@gmail.com>
2021-07-21 17:51:59 +0900

    qnaList 보완

--- vue/src/components/qna/QnaList.vue
+++ vue/src/components/qna/QnaList.vue
@@ -1,16 +1,26 @@
 <template lang="">
-  <el-card class="box-card" @click="showDetail(start.q_idx)">
-    {{ start.contents }}
-  </el-card>
-  <div v-for="q in qList" :key="q.q_idx" class="text item">
-    <el-card class="box-card" @click="showDetail(key)">
-      {{ q.contents }}
-    </el-card>
+  <div>
+    <el-scrollbar height="500px">
+      <!-- 시작 카드 시작 -->
+      <el-card class="box-card" @click="showDetail(start.contents)">
+        {{ start.contents }}
+      </el-card>
+      <!-- 시작 카드 끝 -->
+      <div v-for="q in qList" :key="q.q_idx" class="text item">
+        <el-card class="box-card" @click="showDetail(q.contents)">
+          {{ q.contents }}
+        </el-card>
+      </div>
+      <!-- 종료 카드 시작 -->
+      <el-card class="box-card" @click="showDetail(end.contents)">
+        {{ end.contents }}
+      </el-card>
+      <!-- 종료 카드 끝 -->
+    </el-scrollbar>
   </div>
-  <el-card class="box-card" @click="showDetail(key)">
-    {{ end.contents }}
+  <el-card class="box-card" @click="addScene()">
+    <i class="el-icon-plus"></i>
   </el-card>
-  <el-card class="box-card" @click="addScene()">시나리오 추가</el-card>
 </template>
 <script>
 export default {
@@ -44,16 +54,16 @@ export default {
   methods: {
     showDetail(key) {
       this.qDetail = key;
+      console.log(this.qDetail);
     },
     addScene() {
       let tmp = {
         q_idx: '3',
-        contents: 'dummy',
+        contents: 'dummy' + this.qList.length,
         answers: [],
       };
       if (this.qList.length < 8) this.qList.push(tmp);
       else alert('시나리오는 최대 10개 추가할 수 있습니다.');
-      console.log(this.qList);
     },
   },
 };
