3f7585360d385571d129a0e7850f259f51f7bf2b
한주연 <taxol1203@naver.com>
2021-07-26 15:23:28 +0900

    answer에 연계된 question의 defualt key는 2로 수정

--- backend_source/src/main/java/com/ssafy/d204/db/entity/Answer.java
+++ backend_source/src/main/java/com/ssafy/d204/db/entity/Answer.java
@@ -1,15 +1,21 @@
 package com.ssafy.d204.db.entity;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
 import lombok.Data;
 
 @Data
+@JsonAutoDetect
 public class Answer {
 	int pk_idx;
 	String content;
-	int fk_next_idx;
+	int fk_next_idx = 2;
+
+	public Answer() {
+		super();
+	}
 	
 	public Answer(String content) {
-		super();
 		this.content = content;
 	}
 
@@ -31,5 +37,5 @@ public class Answer {
 		this.pk_idx = pk_idx;
 		this.fk_next_idx = fk_next_idx;
 	}
-	
+
 }
