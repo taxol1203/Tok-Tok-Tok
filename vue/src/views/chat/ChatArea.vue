5bd659183a37bf8d9dfbbba7a4ebe92ddc8e4492
neverte <neverte@naver.com>
2021-07-29 16:55:45 +0900

    애니메이션 효과 적용
    애니메이션 효과 적용

--- vue/src/views/chat/ChatArea.vue
+++ vue/src/views/chat/ChatArea.vue
@@ -6,8 +6,12 @@
         <!-- <el-header v-if="store.state.selected_room"><UserTitle /></el-header> -->
         <!-- <el-main> -->
         <!-- <el-container> -->
-        <el-main v-if="store.state.selected_room"><ChatDetail /></el-main>
-        <el-aside v-if="store.state.selected_room"><UserInfo /></el-aside>
+        <transition name="chat-change" mode="out-in">
+          <el-main v-if="store.state.selected_room"><ChatDetail /></el-main>
+        </transition>
+        <transition name="chat-change" mode="out-in">
+          <el-aside v-if="store.state.selected_room"><UserInfo /></el-aside>
+        </transition>
         <!-- </el-container> -->
         <!-- </el-main> -->
       </el-container>
@@ -15,11 +19,11 @@
   </div>
 </template>
 <script>
-import ChatList from '../../components/chat/ChatList.vue';
+import ChatList from "../../components/chat/ChatList.vue";
 // import UserTitle from '../../components/chat/UserTitle.vue';
-import ChatDetail from '../../components/chat/ChatDetail.vue';
-import UserInfo from '../../components/chat/UserInfo.vue';
-import { useStore } from 'vuex';
+import ChatDetail from "../../components/chat/ChatDetail.vue";
+import UserInfo from "../../components/chat/UserInfo.vue";
+import { useStore } from "vuex";
 
 export default {
   components: {
@@ -29,7 +33,7 @@ export default {
     UserInfo,
   },
   setup() {
-    const test = 'abcdef';
+    const test = "abcdef";
     const store = useStore();
 
     return {
@@ -45,5 +49,25 @@ export default {
 } */
 .el-main {
   padding: 0;
+}
+/* 생성 부분 */
+.chat-change-enter-from {
+  opacity: 0;
+}
+.chat-change-enter-to {
+  opacity: 1;
+}
+.chat-change-enter-active {
+  transition: all 0.7s ease-out;
+}
+/* 소멸 부분 */
+.chat-change-leave-from {
+  opacity: 1;
+}
+.chat-change-leave-to {
+  opacity: 0;
+}
+.chat-change-leave-active {
+  transition: all 1s ease-out;
 }
 </style>
