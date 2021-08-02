73d2de727268ebdfa25134c7757fceea6a5953bc
teemollt <teemollt@gmail.com>
2021-08-02 11:07:24 +0900

    dh test

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,6 +1,6 @@
-import { createStore } from "vuex";
-import axios from "axios";
-import { moduleQna } from "@/store/modules/moduleQna";
+import { createStore } from "vuex"
+import axios from "axios"
+import { moduleQna } from "@/store/modules/moduleQna"
 
 export default createStore({
   modules: { moduleQna },
@@ -12,15 +12,15 @@ export default createStore({
   },
   mutations: {
     GET_ROOMS(state, payload) {
-      state.rooms = payload;
+      state.rooms = payload
       // console.log(payload)
     },
     ADD_ROOMS(state, payload) {
-      state.rooms.push(payload);
+      state.rooms.push(payload)
     },
     PICK_ROOM(state, payload) {
-      state.selected_room = payload;
-      console.log(payload);
+      state.selected_room = payload
+      console.log(payload)
     },
   },
   actions: {
@@ -34,7 +34,7 @@ export default createStore({
         //   `http://localhost:8088/temp/api/chat/rooms/user/${state.user_idx}`
         // );
       } catch (error) {
-        console.log(error);
+        console.log(error)
       }
     },
     async createChatRooms({ commit }) {
@@ -43,17 +43,18 @@ export default createStore({
           unread: 0,
           fk_created_by_idx: 1, // 상담 신청하는 고객의 userid
           fk_client_idx: 1, // 위 필드와 동일값 넣어주면 됨.
-        });
-        console.log(res.data);
-        commit("ADD_ROOMS", res.data);
+        })
+        console.log(res.data)
+        commit("ADD_ROOMS", res.data)
       } catch (error) {
-        console.log(error);
-        alert("채팅방 개설 실패");
+        console.log(error)
+        alert("채팅방 개설 실패")
       }
     },
     pickRoom({ commit }, key) {
-      commit("PICK_ROOM", key);
+      commit("PICK_ROOM", key)
     },
   },
   getters: {},
-});
+})
+//test
