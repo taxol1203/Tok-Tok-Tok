2fc742bf9fa1b948363fd82f524440083d8b5d20
김대연 <daebalprime@gmail.com>
2021-08-02 05:26:07 +0000

    Update index.js

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -26,7 +26,7 @@ export default createStore({
   actions: {
     async getChatRooms({ commit, state }) {
       try {
-        const res = await axios.get(`https://i5d204.p.ssafy.io/api/chat/rooms/user/${state.user_idx}`)
+        const res = await axios.get(`https://i5d204.p.ssafy.io/api/api/chat/rooms/user/${state.user_idx}`)
         console.log(res.data)
         commit('GET_ROOMS', res.data)
 
@@ -39,7 +39,7 @@ export default createStore({
     },
     async createChatRooms({ commit }) {
       try {
-        const res = await axios.post('https://i5d204.p.ssafy.io/api/chat/room', {
+        const res = await axios.post('https://i5d204.p.ssafy.io/api/api/chat/room', {
         // const res = await axios.post("http://localhost:8088/temp/api/chat/room", {
           unread: 0,
           fk_created_by_idx: 1, // 상담 신청하는 고객의 userid
