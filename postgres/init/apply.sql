2b683dbaee016cde9e4a996fceb46cfdb0f1cc2a
한주연 <taxol1203@naver.com>
2021-08-05 11:09:28 +0900

    new sql

--- postgres/init/apply.sql
+++ postgres/init/apply.sql
@@ -1,13 +1,1 @@
-CREATE TABLE "SSAFY_PJT1".chat_session
-(
-    session_id character varying NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    fk_created_by_idx integer NOT NULL,
-    fk_client_idx integer,
-    fk_host_idx integer,
-    fk_permission_idx integer,
-    status character varying(4),
-    unread integer,
-    qna_history character varying(1000) NULL,
-    PRIMARY KEY (session_id)
-);
+ALTER TABLE "SSAFY_PJT1".chat_session ADD COLUMN qna_history character varying(1000) NULL DEFAULT '|';
