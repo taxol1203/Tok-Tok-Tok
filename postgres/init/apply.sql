f1c0191b75011b1b2256c57ed4a262528e202eae
한주연 <taxol1203@naver.com>
2021-08-05 14:37:32 +0900

    change sql link sample to postgres

--- postgres/init/apply.sql
+++ postgres/init/apply.sql
@@ -1,1 +1,1 @@
-ALTER TABLE "SSAFY_PJT1".chat_session ADD COLUMN qna_history character varying(1000) NULL DEFAULT '|';
+ALTER TABLE "SSAFY_PJT1".chat_session ADD COLUMN qna_history character varying(1000) DEFAULT '|';
