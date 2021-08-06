19e64d9898e98740c97b1fdf910ea38d13c6f96f
김소빈 <ksbjjang333@gmail.com>
2021-08-06 14:16:43 +0900

    chatList + userQna

---
+++ vue/src/views/userchat/UserQna.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div></div>
+</template>
+<script>
+import { useStore } from 'vuex';
+import { computed, reactive, ref } from 'vue';
+
+export default {
+  setup() {
+    const store = userStore();
+    const scenes = store.dispatch('');
+  },
+};
+</script>
+<style></style>
