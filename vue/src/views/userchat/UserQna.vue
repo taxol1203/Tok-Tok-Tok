f796a1fb0c1ebe34d442e28d5942914abd904e88
김소빈 <ksbjjang333@gmail.com>
2021-08-06 16:43:11 +0900

    last commit

--- vue/src/views/userchat/UserQna.vue
+++ vue/src/views/userchat/UserQna.vue
@@ -28,7 +28,7 @@
 </template>
 <script>
 import { useStore } from 'vuex';
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 
 export default {
   name: 'Chat',
@@ -46,97 +46,6 @@ export default {
       log,
       chooseAnswer,
     };
-
-    //이전 코드
-    //   const sessionId = computed(() => store.state.selected_room);
-    //   const messages = computed(() => store.getters.get_messages);
-    //   const userName = computed(() => store.state.auth.user.pk_idx);
-    //   const message = ref("");
-    //   let connected = false;
-    //   let stompClient = "";
-
-    //   const connect = () => {
-    //     const serverURL = "https://i5d204.p.ssafy.io/api/chat"; // 서버 채팅 주소
-    //     let socket = new SockJS(serverURL);
-    //     stompClient = Stomp.over(socket);
-    //     console.log(`connecting to socket=> ${serverURL}`);
-    //     stompClient.connect(
-    //       {},
-    //       (frame) => {
-    //         connected = true;
-    //         console.log("CONNECT SUCCESS ++ status : established", frame);
-    //         // 구독 == 채팅방 입장.
-    //         stompClient.subscribe("/send/" + sessionId.value, (res) => {
-    //           console.log("receive from server:", res.body);
-    //           store.commit("MESSAGE_PUSH", JSON.parse(res.body)); // 수신받은 메세지 표시하기
-    //           switch (res.body.type) {
-    //             case 'MSG':
-    //               break;
-    //             case 'JOIN':
-    //               // 방을 생성할 때 백엔드단에서 처리하므로 신경 x
-    //               break;
-    //             case 'QUIT':
-    //               // 만약 둘 중 하나가 나가면 더 이상 채팅을 못치는 프론트구현
-    //               break;
-    //             case 'VID':
-    //               // vid 시작시 -> 화상채팅 시작하기 버튼만 딸랑 띄우기
-    //               break;
-    //             default:
-    //               // 알수없는 오류...
-    //               break;
-    //           }
-    //         });
-    //       },
-    //       (error) => {
-    //         // 소켓 연결 실패
-    //         console.log('status : failed, STOMP CLIENT 연결 실패', error);
-    //         connected = false;
-    //       }
-    //     );
-    //   };
-    //   connect(sessionId.value);
-
-    //   const sendMessage = () => {
-    //     if (userName.value !== "" && message.value !== "") {
-    //       // 이벤트 발생 엔터키 + 유효성 검사는 여기에서
-    //       send({ message: message }); // 전송 실패 감지는 어떻게? 프론트단에서 고민좀 부탁 dream
-    //     }
-    //     message.value = '';
-    //   };
-
-    //   const send = () => {
-    //     console.log("Send message:" + message.value);
-    //     if (userName.value <= 0) {
-    //       console.log("0이하면 안됨) fk_author_idx: " + userName.value);
-    //     }
-    //     //DB에 없는 유저 idx(0같은 것)가 들어가면 안된다.
-    //     if (stompClient && stompClient.connected && userName.value > 0) {
-    //       console.log("IN SOCKET");
-    //       const msg = {
-    //         message: message.value, // 메세지 내용. type이 MSG인 경우를 제외하곤 비워두고 프론트단에서만 처리.
-    //         fk_author_idx: userName.value, // 작성자의 회원 idx
-    //         created: "", // 작성시간, 공란으로 비워서 메세지 보내기. response에는 담겨옵니다.
-    //         deleted: false, // 삭제된 메세지 여부. default = false
-    //         fk_session_id: sessionId.value, // 현재 채팅세션의 id.
-    //         // 주의할 점은, 방 세션 id가 아닌, 방 정보의 pk_idx를 첨부한다. created 라이프사이클 메서드 참조.
-    //         type: 'MSG', // 메세지 타입.
-    //       };
-    //       stompClient.send('/receive/' + sessionId.value, JSON.stringify(msg), {});
-    //     }
-    //   };
-
-    //   return {
-    //     store,
-    //     sessionId,
-    //     messages,
-    //     message,
-    //     sendMessage,
-    //     send,
-    //     connect,
-    //     connected,
-    //     stompClient,
-    //     userName,
-    //   };
   },
 };
 </script>
@@ -148,7 +57,7 @@ export default {
 #topMessages {
   display: block;
   top: 0px;
-  height: 700px;
+  height: 300px;
   width: 100%;
 }
 .el-scroll {
