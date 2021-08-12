4939c6ff599de2d99f18aaac02f3d278c687a36d
한주연 <taxol1203@naver.com>
2021-08-12 14:57:21 +0900

    jenkins test

--- backend_source/src/main/java/com/ssafy/d204/api/controller/QnAController.java
+++ backend_source/src/main/java/com/ssafy/d204/api/controller/QnAController.java
@@ -26,6 +26,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// jenkins test
+
 @CrossOrigin(origins = {"*"}, maxAge = 6000)
 @RestController
 @RequestMapping("/qna")
